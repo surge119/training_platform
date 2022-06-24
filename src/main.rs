715534d9d6a29bd7d40dd5b6388c828edfe2a123
Sly480 <ly.sergio480@gmail.com>
2022-06-24 11:32:06 -0400

    Updates for testing, etc

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use std::thread;
+use std::time::Duration;
 use bollard::Docker;
+
 mod docker;
-fn main() {
-    //let docker_container = Docker::connect_with_local_defaults().unwrap();
-}
-#[test]
-fn docker_test_1(){
+
+#[tokio::main]
+async fn main() {
+    let docker_container = Docker::connect_with_local_defaults().unwrap();
+    let image: &str = "ubuntu";
+    let name: &str = "test_bollard";
+    docker::create_docker_container(&docker_container, image, name).await;
+    docker::start_docker_container(&docker_container, name).await;
+    // docker::remove_docker_container(&docker_container, name).await;
 
 }
