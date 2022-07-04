dadaa38976dd9c4cbadce0768359174594d2c8b6
Powall <powallwang@gmail.com>
2022-07-04 15:59:55 -0400

    fixed network functions and did more testing

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,13 @@ mod docker;
 async fn main() {
     let docker_container = Docker::connect_with_local_defaults().unwrap();
     let image: &str = "ubuntu";
-    let name: &str = "test_bollard";
-    docker::create_docker_container(&docker_container, image, name).await;
-    docker::start_docker_container(&docker_container, name).await;
-    // docker::remove_docker_container(&docker_container, name).await;
-
+    let network_name: &str = "test_network";
+    let container_name: &str = "test_container";
+    let subnet: String = "192.168.0.0/16".to_string();
+    //docker::create_docker_container(&docker_container, image, container_name).await;
+    //docker::start_docker_container(&docker_container, container_name).await;
+    //docker::stop_docker_container(&docker_container, container_name).await;
+    //docker::start_docker_network(&docker_container, network_name, subnet, true).await;
+    //docker::connect_docker_network(&docker_container, network_name, container_name, "192.168.0.2").await;
+    //docker::disconnect_docker_network(&docker_container, network_name, container_name).await;
 }
