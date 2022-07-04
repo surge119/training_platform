d6417cac612d9e915cd83d94fa4c990c07d9b094
Sly480 <ly.sergio480@gmail.com>
2022-07-04 17:36:23 -0400

    Reformatted code

--- src/docker.rs
+++ src/docker.rs
@@ -1,57 +1,73 @@
-use bollard::Docker;
-use bollard::container::{CreateContainerOptions, Config};
-use bollard::container::StartContainerOptions;
 use bollard::container::RemoveContainerOptions;
-use bollard::network::CreateNetworkOptions;
-use bollard::network::ConnectNetworkOptions;
-use bollard::network::DisconnectNetworkOptions;
-use bollard::models::{EndpointSettings, EndpointIpamConfig, Ipam};
+use bollard::container::StartContainerOptions;
+use bollard::container::{Config, CreateContainerOptions};
 use bollard::models::IpamConfig;
+use bollard::models::{EndpointIpamConfig, EndpointSettings, Ipam};
+use bollard::network::ConnectNetworkOptions;
+use bollard::network::CreateNetworkOptions;
+use bollard::network::DisconnectNetworkOptions;
+use bollard::Docker;
 
 use std::default::Default;
 
 /// Create a new docker container
-pub async fn create_docker_container(docker: &Docker, image_name: &str, container_name: &str){
-    let options = Some(CreateContainerOptions{
+pub async fn create_docker_container(docker: &Docker, image_name: &str, container_name: &str) {
+    let options = Some(CreateContainerOptions {
         name: container_name,
     });
-    
+
     let config = Config {
         image: Some(image_name),
         ..Default::default()
     };
-    
-    docker.create_container(options, config).await.expect("container failed to be created");
+
+    docker
+        .create_container(options, config)
+        .await
+        .expect("container failed to be created");
 }
 
 /// Remove an existing docker container
-pub async fn remove_docker_container(docker: &Docker, container_name: &str){
-    let options = Some(RemoveContainerOptions{
+pub async fn remove_docker_container(docker: &Docker, container_name: &str) {
+    let options = Some(RemoveContainerOptions {
         force: true,
         ..Default::default()
     });
 
-    docker.remove_container(container_name, options).await.expect("container failed to be removed");
+    docker
+        .remove_container(container_name, options)
+        .await
+        .expect("container failed to be removed");
 }
 
-pub async fn start_docker_container(docker: &Docker, container_name: &str){
-    docker.start_container(container_name, None::<StartContainerOptions<String>>).await.expect("container failed to start");
+pub async fn start_docker_container(docker: &Docker, container_name: &str) {
+    docker
+        .start_container(container_name, None::<StartContainerOptions<String>>)
+        .await
+        .expect("container failed to start");
 }
 //was able to test by running docker run -t -d IMAGENAME and then running this function
-pub async fn stop_docker_container(docker: &Docker, container_name: &str){
-    docker.stop_container(container_name, None).await.expect("container failed to stop");
+pub async fn stop_docker_container(docker: &Docker, container_name: &str) {
+    docker
+        .stop_container(container_name, None)
+        .await
+        .expect("container failed to stop");
 }
 
-pub async fn start_docker_network(docker: &Docker, network_name:&str, subnet:String, is_attachable:bool){
-
-    let ipam = IpamConfig{
+pub async fn start_docker_network(
+    docker: &Docker,
+    network_name: &str,
+    subnet: String,
+    is_attachable: bool,
+) {
+    let ipam = IpamConfig {
         subnet: Some(subnet),
         ..Default::default()
     };
     let ipam_configs = vec![ipam];
     let config = CreateNetworkOptions {
         name: network_name,
-        ipam: Ipam{
+        ipam: Ipam {
             config: Some(ipam_configs),
             ..Default::default()
         },
@@ -59,30 +75,44 @@ pub async fn start_docker_network(docker: &Docker, network_name:&str, subnet:Str
         ..Default::default()
     };
 
-    docker.create_network(config).await.expect("network failed to start");
- }
+    docker
+        .create_network(config)
+        .await
+        .expect("network failed to start");
+}
 
 //docker container must be running for this to work
-pub async fn connect_docker_network(docker:&Docker, network_name:&str, container_name:&str, ip_address:&str){
+pub async fn connect_docker_network(
+    docker: &Docker,
+    network_name: &str,
+    container_name: &str,
+    ip_address: &str,
+) {
     let config = ConnectNetworkOptions {
-                container: container_name,
-                endpoint_config: EndpointSettings {
-                ipam_config: Some(EndpointIpamConfig {
+        container: container_name,
+        endpoint_config: EndpointSettings {
+            ipam_config: Some(EndpointIpamConfig {
                 ipv4_address: Some(String::from(ip_address)),
                 ..Default::default()
             }),
             ..Default::default()
-        }     
+        },
     };
 
-    docker.connect_network(network_name, config).await.expect("failed to connect to network");
+    docker
+        .connect_network(network_name, config)
+        .await
+        .expect("failed to connect to network");
 }
 
-pub async fn disconnect_docker_network(docker:&Docker, network_name:&str, container_name: &str){
+pub async fn disconnect_docker_network(docker: &Docker, network_name: &str, container_name: &str) {
     let config = DisconnectNetworkOptions {
-       container: container_name,
-        force: true
+        container: container_name,
+        force: true,
     };
 
-    docker.disconnect_network(network_name, config).await.expect("failed to disconnnect from network");
+    docker
+        .disconnect_network(network_name, config)
+        .await
+        .expect("failed to disconnnect from network");
 }
--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,28 @@ mod docker;
 #[derive(Serialize)]
 struct Box {
     name: String,
-    instances: u32
+    instances: u32,
 }
 #[derive(Serialize)]
 struct Status {
     boxes: bool,
     used_instances: u8,
-    available_instances: u8
+    available_instances: u8,
 }
-async fn get_boxes()  -> impl Responder {
+async fn get_boxes() -> impl Responder {
     //This will do some actual checking of boxes and their respective status
-    return web::Json(Box{name:String::from("test"),instances:1});
+    return web::Json(Box {
+        name: String::from("test"),
+        instances: 1,
+    });
 }
 
 async fn check_server_health() -> impl Responder {
-    return web::Json(Status{boxes:true,used_instances: 0, available_instances: 0});
+    return web::Json(Status {
+        boxes: true,
+        used_instances: 0,
+        available_instances: 0,
+    });
 }
 
 #[actix_web::main]
@@ -28,10 +35,10 @@ async fn main() -> std::io::Result<()> {
         App::new().service(
             web::scope("/api")
                 .route("/boxes", web::get().to(get_boxes))
-                .route("/status", web::get().to(check_server_health))
-            )
+                .route("/status", web::get().to(check_server_health)),
+        )
     })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
