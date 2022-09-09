b84a0850d8db87296102eefa07bf15b940bd23de
Sergio <ly.sergio480@gmail.com>
2022-09-09 10:11:15 -0400

    Exposed challenges for testing

--- setup.sh
+++ setup.sh
@@ -12,3 +12,7 @@ sudo systemctl stop docker.socket
 
 # Start docker daemon and listen on 10.10.10.1
 sudo dockerd -H unix:///var/run/docker.sock -H tcp://10.10.10.1 > dockerd-logs &
+
+# Add current user to docker group
+sudo usermod -aG docker $(whomai)
+
