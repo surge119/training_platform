1da41c348d6708a177013a3a3e41c0a71b4e917b
Sly480 <ly.sergio480@gmail.com>
2022-09-12 06:54:57 -0400

    Updated setup script

---
+++ setup/rust_server.py
@@ -0,0 +1,40 @@
+import docker
+import json
+import shutil
+
+
+def setup_rust_server(challenges_path: str):
+    """Build the rust server and it's config
+
+    Args:
+        challenges_path: The path to the docker compose for the challenges
+
+    Returns: None
+
+    """
+    # Generate configuration file
+    config_file = "rust-server/config.json"
+    generate_config(config_file)
+
+    # Copy challenges compose file to rust-server directory
+    tmp_compose = "./rust-server/docker-compose.yml.tp"
+    shutil.copyfile(challenges_path, tmp_compose)
+
+
+def generate_config(config_file: str):
+    """Generate a config file for the rust server
+
+    Args:
+        config_file: The config file path
+
+    Returns: None
+
+    """
+    client = docker.from_env()
+    api_version = client.version()["Components"][0]["Details"]["ApiVersion"]
+    config = {
+        "API_VERSION_MAJOR": api_version[0],
+        "API_VERSION_MINOR": api_version[2:],
+    }
+    with open(config_file, "w") as server_config:
+        json.dump(config, server_config)
